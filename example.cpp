f6c83b67b4d0121539042fd582e23686f15fd439
trodevel <trodevel@gmail.com>
2020-04-23 04:00:57 +0200

    extended example

--- example.cpp
+++ example.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <sstream>
 
 #include "exported_parser.h"
+#include "exported_csv_helper.h"
 
 int main()
 {
+    std::ostringstream os;
+
+    basic_parser::csv_encoder::write( os, "hello char" );
+
+    basic_parser::csv_encoder::write( os, std::string( "hello std::string" ) );
+
+    std::cout << "example: " << os.str() << std::endl;
+
     return 0;
 }
