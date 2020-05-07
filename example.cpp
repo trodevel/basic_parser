25cae19bc70ce5203a5dbc33187ba0191e1dac12
trodevel <trodevel@gmail.com>
2020-05-07 02:42:13 +0200

    moved exported_csv_helper to csv_helper

--- example.cpp
+++ example.cpp
@@ -2,15 +2,15 @@
 #include <sstream>
 
 #include "exported_parser.h"
-#include "exported_csv_helper.h"
+#include "csv_helper.h"
 
 int main()
 {
     std::ostringstream os;
 
-    basic_parser::csv_encoder::write( os, "hello char" );
+    basic_parser::csv_helper::write( os, "hello char" );
 
-    basic_parser::csv_encoder::write( os, std::string( "hello std::string" ) );
+    basic_parser::csv_helper::write( os, std::string( "hello std::string" ) );
 
     std::cout << "example: " << os.str() << std::endl;
 
