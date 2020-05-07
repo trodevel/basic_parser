ac8c96d99d57739fec95f52aa2e012634a63a611
trodevel <trodevel@gmail.com>
2020-05-07 02:42:36 +0200

    moved exported_csv_helper to csv_helper

---
+++ csv_helper.cpp
@@ -0,0 +1,96 @@
+/*
+
+CsvHelper.
+
+Copyright (C) 2020 Sergey Kolevatov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// $Revision: 12985 $ $Date:: 2020-05-07 #$ $Author: serge $
+
+// includes
+#include "csv_helper.h"
+#include "utils/csv_helper.h"
+
+namespace basic_parser
+{
+
+namespace csv_helper
+{
+
+std::ostream & write( std::ostream & os, const bool r )
+{
+    return utils::CsvHelper::write( os, r );
+}
+
+std::ostream & write( std::ostream & os, const int8_t r )
+{
+    return utils::CsvHelper::write( os, r );
+}
+
+std::ostream & write( std::ostream & os, const uint8_t r )
+{
+    return utils::CsvHelper::write( os, r );
+}
+
+std::ostream & write( std::ostream & os, const int16_t r )
+{
+    return utils::CsvHelper::write( os, r );
+}
+
+std::ostream & write( std::ostream & os, const uint16_t r )
+{
+    return utils::CsvHelper::write( os, r );
+}
+
+std::ostream & write( std::ostream & os, const int32_t r )
+{
+    return utils::CsvHelper::write( os, r );
+}
+
+std::ostream & write( std::ostream & os, const uint32_t r )
+{
+    return utils::CsvHelper::write( os, r );
+}
+
+std::ostream & write( std::ostream & os, const int64_t r )
+{
+    return utils::CsvHelper::write( os, r );
+}
+
+std::ostream & write( std::ostream & os, const uint64_t r )
+{
+    return utils::CsvHelper::write( os, r );
+}
+
+std::ostream & write( std::ostream & os, const float r )
+{
+    return utils::CsvHelper::write( os, r );
+}
+
+std::ostream & write( std::ostream & os, const double r )
+{
+    return utils::CsvHelper::write( os, r );
+}
+
+std::ostream & write( std::ostream & os, const std::string & r )
+{
+    return utils::CsvHelper::write( os, r );
+}
+
+} // namespace csv_helper
+
+} // namespace basic_parser
---
+++ csv_helper.h
@@ -0,0 +1,82 @@
+/*
+
+CsvHelper.
+
+Copyright (C) 2020 Sergey Kolevatov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// $Revision: 12985 $ $Date:: 2020-05-07 #$ $Author: serge $
+
+#ifndef LIB_BASIC_PARSER__CSV_HELPER_H
+#define LIB_BASIC_PARSER__CSV_HELPER_H
+
+// system includes
+#include <sstream>
+#include <vector>
+#include <map>
+
+namespace basic_parser
+{
+
+namespace csv_helper
+{
+
+std::ostream & write( std::ostream & os, const bool r );
+std::ostream & write( std::ostream & os, const int8_t r );
+std::ostream & write( std::ostream & os, const uint8_t r );
+std::ostream & write( std::ostream & os, const int16_t r );
+std::ostream & write( std::ostream & os, const uint16_t r );
+std::ostream & write( std::ostream & os, const int32_t r );
+std::ostream & write( std::ostream & os, const uint32_t r );
+std::ostream & write( std::ostream & os, const int64_t r );
+std::ostream & write( std::ostream & os, const uint64_t r );
+std::ostream & write( std::ostream & os, const float r );
+std::ostream & write( std::ostream & os, const double r );
+std::ostream & write( std::ostream & os, const std::string & r );
+
+template <class T, class W >
+std::ostream & write_t( std::ostream & os, const std::vector<T> & r, W writer )
+{
+    os << "sz:" << r.size() << " ";
+
+    for( auto & e : r )
+    {
+        writer( os, e );
+    }
+
+    return os;
+}
+
+template <class U, class V, class W1, class W2>
+std::ostream & write_t( std::ostream & os, const std::map<U,V> & r, W1 writer1, W2 writer2 )
+{
+    os << "sz:" << r.size() << " ";
+
+    for( auto & e : r )
+    {
+        writer1( os, e.first );
+        writer2( os, e.second );
+    }
+
+    return os;
+}
+
+} // namespace csv_helper
+
+} // namespace basic_parser
+
+#endif // LIB_BASIC_PARSER__CSV_HELPER_H
