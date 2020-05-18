d43210b2a890cd3a27d0e03982908d11b030a4ec
trodevel <trodevel@gmail.com>
2020-05-18 02:39:18 +0200

    PHP: added str_helper

---
+++ str_helper.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+
+Str helper.
+
+Copyright (C) 2020 Sergey Kolevatov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// $Revision: 13085 $ $Date:: 2020-05-18 #$ $Author: serge $
+
+namespace basic_parser;
+
+function to_string__bool( $v )
+{
+    $res = ( $v ) ? "TRUE" : "FALSE";
+
+    return $res;
+}
+
+function to_string__int( $v )
+{
+    $res = $v + 0;
+
+    return $res;
+}
+
+function to_string__float( $v )
+{
+    $res = $v + 0.0;
+
+    return $res;
+}
+
+function to_string__string( $v )
+{
+    $res = $v;
+
+    return $res;
+}
+
+function to_string__vector( $vect, $func )
+{
+    $res = "";
+
+    foreach( $vect as $v )
+    {
+        $val = $func( $v );
+
+        $res .=  $val;
+    }
+
+    return $res;
+}
+
+function to_string__map( $map, $func1, $func2 )
+{
+    $res = "";
+
+    foreach( $vect as $v_k => $v_v )
+    {
+        $key = $func1( $v_k );
+
+        $val = $func2( $v_v );
+
+        $res .=  "(" . $key . " -> " . $val . ")";
+    }
+
+    return $res;
+}
+
+?>
