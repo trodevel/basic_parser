739865d780a7dad6e1c8eac9d889134a33c9d2c6
trodevel <trodevel@gmail.com>
2020-06-06 02:26:54 +0200

    CPP: added object

---
+++ object.h
@@ -0,0 +1,37 @@
+/*
+
+Object.
+
+Copyright (C) 2020 Sergey Kolevatov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// $Revision: 13199 $ $Date:: 2020-06-06 #$ $Author: serge $
+
+#ifndef LIB_BASIC_PARSER__OBJECT_H
+#define LIB_BASIC_PARSER__OBJECT_H
+
+namespace basic_parser
+{
+
+struct Object
+{
+    virtual ~Object() {}
+};
+
+} // namespace basic_parser
+
+#endif // LIB_BASIC_PARSER__OBJECT_H
